3e2524947a5d5289e03207e217f53d627ecff6ee
Zhyar <zhiarali595@gmail.com>
2026-01-22 19:36:31 +0300

    add add-book functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const library = [];
+const booksDisplayed = [];
 
 function Book(id, title, author, pages, status){
     if(!new.target){
@@ -31,28 +32,58 @@ const CONTAINER = document.querySelector(".container");
 
 function displayBooks(){
     for (const book of library){
-        const div = document.createElement("div");
-        div.classList.add("card");
+        if(!booksDisplayed.includes(book)){
+            const div = document.createElement("div");
+            div.classList.add("card");
 
-        const paraOne = document.createElement("p");
-        const upper = document.createTextNode(book.title + " by " + book.author);
-        paraOne.appendChild(upper);
-        paraOne.classList.add("upper");
+            const paraOne = document.createElement("p");
+            const upper = document.createTextNode(book.title + " by " + book.author);
+            paraOne.appendChild(upper);
+            paraOne.classList.add("upper");
 
-        const paraTwo = document.createElement("p");
-        const lower = document.createTextNode(book.pages + " pages");
-        paraTwo.appendChild(lower);
-        paraTwo.classList.add("lower");
+            const paraTwo = document.createElement("p");
+            const lower = document.createTextNode(book.pages + " pages");
+            paraTwo.appendChild(lower);
+            paraTwo.classList.add("lower");
 
-        div.append(paraOne,paraTwo);
-        CONTAINER.appendChild(div);
+            const paraThree = document.createElement("p");
+            const lowest = document.createTextNode("Status: " + book.status);
+            paraThree.appendChild(lowest);
+            paraThree.classList.add("lowest");
+
+            div.append(paraOne,paraTwo,paraThree);
+            CONTAINER.appendChild(div);
+
+            booksDisplayed.push(book);
+        }
     }
 }
 displayBooks();
 
 const addButton = document.getElementById("add");
 const dialog = document.getElementById("form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const statusinput = document.querySelector('input[name="status"]:checked');
 
 addButton.addEventListener("click", () =>{
     dialog.showModal();
 });
+
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", submitClick);
+
+function submitClick(event){
+    event.preventDefault();
+
+    const titleValue = title.value;
+    const authorValue = author.value;
+    const pagesValue = pages.value;
+    const statusValue = document.querySelector('input[name="status"]:checked').value;
+
+    addBookToLibrary(titleValue, authorValue, pagesValue, statusValue);
+    displayBooks();
+}
+
